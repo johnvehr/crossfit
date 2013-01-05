8721ef747815bd7ef72f23d7fb797318eaaa9fc1
John Vehr <vehr@kyck.com>
2013-01-05 15:15:12 -0500

    some embeded ruby, css with some words not spelled right and some embeded javascript, basically this is a hack and needs cleaning

--- app/assets/javascripts/custom704.js
+++ app/assets/javascripts/custom704.js
@@ -1,8 +1,7 @@
-//few javascript functions for crossfit704
+//few javascript functions for crossfit704, yes few...very few
 
 
 function toMap() {
   window.scrollTo(0, document.body.scrollHeight);
 }
-
 
